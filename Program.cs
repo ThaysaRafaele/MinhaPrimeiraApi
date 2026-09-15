7f32d9595821276b64b505817e7960c8eb43b4b3
Thaysa <thaysarafaele@gmail.com>
2026-09-15 14:28:33 -0400

    feat: adiciona migrations e banco SQLite

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using MinhaPrimeiraApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=minhaapi.db"));
+
 
 var app = builder.Build();
 
